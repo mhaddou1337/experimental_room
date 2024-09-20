3e8a805318fe1f430f3bbc31b1b68bb5f1c4b9d1
yavi337 <165167690+yavi337@users.noreply.github.com>
2024-09-20 02:58:04 +0200

    first time with Python

---
+++ Python/py_syntax.py
@@ -0,0 +1,13 @@
+print("hello")
+#mdrcest lang tres sample frere
+x = 1
+y = "Hoe meer ik leer, hoe meer ik besef dat ik niets weet!!!"
+print(y)
+
+print(x,'"sqeaz"' "\ntt" "\tz")
+print('\0')
+
+'''
+Commentaar moet je plaatsen tussen \'\'\'
+dsfds
+'''
