97c506472eed4239d42a4c47bac70dcfea7b2500
yavi337 <165167690+yavi337@users.noreply.github.com>
2024-09-20 03:19:57 +0200

    Update py_syntax.py

--- Python/py_syntax.py
+++ Python/py_syntax.py
@@ -11,3 +11,18 @@ print('\0')
 Commentaar moet je plaatsen tussen \'\'\'
 dsfds
 '''
+y = str(1) #y will be (char or string) when u try to know type use function type  it will show you <class 'str'>
+#The type() function is used to get the type of an object!
+print(type(y))
+a = b = c = d = "alpha"
+print(d)
+
+cars = ["BMW", "DACIA", "BENZ"]
+g,h,j = cars #in this case g == whats inside index etc
+print(10+10) # we can't add an int to str!
+
+def test_f():
+  print(y)
+  global d = 5 #global make used to make global var
+
+myfunc()
