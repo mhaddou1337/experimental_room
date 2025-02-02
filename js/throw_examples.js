187959245fabfedaa5814e57d5b3d6af06a454f9
mounir <165167690+mhaddou1337@users.noreply.github.com>
2025-02-02 21:04:53 +0000

    Update throw_examples.js

--- js/throw_examples.js
+++ js/throw_examples.js
@@ -6,6 +6,23 @@ function usr_mail_checker(username,email)
         throw new Error("Enter a valid Email");
     console.log("accounts have been created");
 }
-
-
 usr_mail_checker("s", "s@s.com");
+
+const n_checker = function(x , y)
+    {
+        if(x !==y)
+            return true;
+        if (x === y)
+            throw new Error("x et y sont pas egales");
+    }
+fvvv(5,5);
+
+sum_checker(12,8,20);
+function sum_checker(x,y,total)
+{
+    if(x+y === total)
+    return(true);
+    else if (x+y !== total)
+        throw new Error(`cest pas la meme valuer, abs cest ${Math.abs(total-(x+y))}`)
+}
+
